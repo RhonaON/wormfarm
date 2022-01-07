cfdfbe6ab17d7121e1413bc39c451d767b2cc4cb
Rhona O'Neill <rhonaon@gmail.com>
2022-01-07 13:15:17 +1300

    add font. Start using chakra components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+import '@fontsource/ibm-plex-mono/400.css'
+import '@fontsource/ibm-plex-mono/700.css'
+
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../theme'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,32 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import {
+  Container,
+  Stack,
+  Heading,
+  Text,
+} from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>Worm Farm</title>
         <meta name="description" content="A happy place for happy worms" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>
-          Welcome to Worm Farm!
-        </h1>
-      </main>
-    </div>
+      <Container>
+        <Stack>
+          <Heading>
+            Welcome to Worm Farm!
+          </Heading>
+          <Text>
+            So apparently... Charlie is a cat. A really good cat you see.
+          </Text>
+        </Stack>
+      </Container>
+    </>
   )
 }
 
